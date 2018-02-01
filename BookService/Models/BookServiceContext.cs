d1446a1554746664c0c20c504c5c7e1cc1945204
Jian Long Yaw <yaw.jian.long@exact.com>
2018-02-01 22:41:09 +0800

    Create Web API Model classes and Controllers
    - Add web api v2 model classes and controllers for book service
    - Model -> Author, Book

---
+++ BookService/Models/BookServiceContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace BookService.Models
+{
+	public class BookServiceContext : DbContext
+	{
+		public BookServiceContext() : base("name=BookServiceContext")
+		{
+		}
+
+		public DbSet<Author> Authors { get; set; }
+
+		public DbSet<Book> Books { get; set; }
+	}
+}
